01d90fd901f3f16ff3ca6bb2704a3f522be88cb8
sonny070701 <91562352+sonny070701@users.noreply.github.com>
2021-10-25 17:02:19 -0500

    Add files via upload

---
+++ src/ALU.v
@@ -0,0 +1,68 @@
+/******************************************************************
+* Description
+*	This is an 32-bit arithetic logic unit that can execute the next set of operations:
+*		add
+
+* This ALU is written by using behavioral description.
+* Version:
+*	1.0
+* Author:
+*	Dr. José Luis Pizano Escalante
+* email:
+*	luispizano@iteso.mx
+* Date:
+*	16/08/2021
+******************************************************************/
+
+module ALU 
+(
+	input [3:0] ALU_Operation_i,
+	input signed [31:0] A_i,
+	input signed [31:0] B_i,
+	output reg Zero_o,
+	output reg [31:0] ALU_Result_o
+);
+
+localparam ADD  = 4'b0000;
+localparam SUB  = 4'b0001;
+localparam LUI  = 4'b0010;
+localparam ORI  = 4'b0011;
+localparam SLLI = 4'b0100;
+localparam SRLI = 4'b0101; 
+localparam AND	 = 4'b0110;
+   
+   always @ (A_i or B_i or ALU_Operation_i)
+     begin
+		case (ALU_Operation_i)
+			ADD:
+				ALU_Result_o = A_i + B_i;
+
+			SUB:
+				ALU_Result_o = A_i - B_i;
+		
+			LUI:
+				ALU_Result_o = B_i << 12;
+			
+			ORI:
+				ALU_Result_o = A_i | B_i;
+				
+			SLLI:
+				ALU_Result_o = A_i << B_i;
+				
+			SRLI:
+				ALU_Result_o = A_i >> B_i;
+				
+			AND:
+				ALU_Result_o = A_i & B_i;
+				
+			
+	
+		default:
+			ALU_Result_o = 0;
+		endcase // case(control)
+		
+		Zero_o = (ALU_Result_o == 0) ? 1'b1 : 1'b0;
+		
+     end // always @ (A or B or control)	  
+	  	  
+endmodule // ALU
---
+++ src/ALU_Control.v.bak
@@ -0,0 +1,44 @@
+/******************************************************************
+* Description
+*	This is the control unit for the ALU. It receves a signal called 
+*	ALUOp from the control unit and signals called funct7 and funct3  from
+*	the instruction bus.
+* Version:
+*	1.0
+* Author:
+*	Dr. José Luis Pizano Escalante
+* email:
+*	luispizano@iteso.mx
+* Date:
+*	16/08/2021
+******************************************************************/
+module ALU_Control
+(
+	input funct7_i, 
+	input [2:0] ALU_Op_i,
+	input [2:0] funct3_i,
+	
+
+	output [3:0] ALU_Operation_o
+
+);
+
+reg [3:0] alu_control_values;
+wire [6:0] selector;
+
+assign selector = {funct7_i, ALU_Op_i, funct3_i};
+
+always@(selector)begin
+	casex(selector)
+	
+
+		default: alu_control_values = 4'b00_00;
+	endcase
+end
+
+
+assign ALU_Operation_o = alu_control_values;
+
+
+
+endmodule
